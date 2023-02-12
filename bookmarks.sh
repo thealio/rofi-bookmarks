24e4db02112576323b23183c77e9d77c83131b13
thealio <116115143+thealio@users.noreply.github.com>
2023-02-12 20:56:27 +0000

    Update bookmarks.sh
    - Made the  more usable if you wants to adapt it to fzf or dmenu
    - avoid google, change to duckduckgo instead

--- bookmarks.sh
+++ bookmarks.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
-
+#
+# Origilally from
+# https://github.com/Strix007/rofi-bookmarks
+#
+# USAGE ############################################################################################################################################################
+#
+# - Add Bookmark
+#
+# To add a bookmark, start the line with + then your url. Default browser is settetd in your envirorment variables ($BROWSER) .
+# Note: If your bookmark does not contain a domain extension, it will default to .com .
+#
+# - Remove Bookmark
+#
+# To remove a bookmark, start the line with _ then your url .
+# Note: It fuzzy matches the input to the saved bookmarks .
+#
+# - Addition Usage
+#
+# If you type a address that is not saved in your saved bookmarks, it will open the browser with that address .
+# Note: If the address does not contain a domain extension, it will default to a google search. 
+#
+# - Saved Bookmarks
+#
+# The bookmarks are saved in a hidden plain text file in $HOME/.scripts/rofi/rofi-webookmarks/bookmarks .
+# You can manually add bookmarks to $HOME/.scripts/rofi/rofi-webookmarks/bookmarks by starting a new line with \n then your bookmark . For Example - \nyoutube.com .
+# Note: Make sure every bookmark is on a seperate line .
+#
+#################################################################################################################################################################
+#
 DIR="$HOME/.config/rofi/bookmarks/"
 THEME="bookmarks"
 BOOKMARKS_FILE="$HOME/.config/rofi/bookmarks/.bookmarks"
-BROWSER="firefox"
+# Note: the default browser is setted by the envirorment varieble $BROWSER
+# You can change in uncommenting the following line (the example browser is "firefox")
+# BROWSER="firefox"
 
 # Check if there is a bookmarks file and if not, make one
 
@@ -11,14 +41,14 @@ if [[ ! -a "${BOOKMARKS_FILE}" ]]; then
     touch "${BOOKMARKS_FILE}"
 fi
 
-INPUT=$(echo -e $(cat $BOOKMARKS_FILE) | rofi -dmenu -theme ${DIR}/${THEME}.rasi -p "")
+INPUT=$(cat $BOOKMARKS_FILE | rofi -dmenu -theme ${DIR}/${THEME}.rasi -p "")
 
 if   [[ $INPUT == "+"* ]]; then
     INPUT=$(echo $INPUT | sed 's/+//') 
     if [[ $INPUT == *"."* ]]; then
-        echo "\n$INPUT" >> $BOOKMARKS_FILE
+        echo "$INPUT" >> $BOOKMARKS_FILE
     else 
-        INPUT="${INPUT}.com" && echo "\n$INPUT" >> $BOOKMARKS_FILE
+        INPUT="${INPUT}.com" && echo "$INPUT" >> $BOOKMARKS_FILE
     fi
 elif [[ $INPUT == "_"* ]]; then
     INPUT=$(echo $INPUT | sed 's/_//') && sed -i "/$INPUT/d" $BOOKMARKS_FILE
@@ -27,5 +57,5 @@ elif [[ $INPUT == *"."* ]]; then
 elif [[ -z $INPUT  ]]; then
     exit 0
 else
-    $BROWSER --search $INPUT
+    $BROWSER "http://www.duckduckgo.com/search?q=$INPUT"
 fi
